503a0ae199963c15235b9ceabec38d4ff6be37c5
emilia-t <sophietwilight@qq.com>
2026-05-05 10:57:32 +0800

    add action loop

--- atswsite/src/components/pixel_war/class/index.ts
+++ atswsite/src/components/pixel_war/class/index.ts
@@ -1,66 +1,75 @@
 ﻿export { CursorManager } from './CursorManager/CursorManager';
 export { EffectManager } from './EffectManager/EffectManager';
 export { Entity } from './Entity/Entity';
-export { EmptyEntity } from './Entity/EmptyEntity/EmptyEntity';
-export { StaticEntity } from './Entity/StaticEntity/StaticEntity';
-export { BoxStaticEntity } from './Entity/StaticEntity/BoxStaticEntity/BoxStaticEntity';
-export { WallStaticEntity } from './Entity/StaticEntity/WallStaticEntity/WallStaticEntity';
-export { CurbStaticEntity } from './Entity/StaticEntity/CurbStaticEntity/CurbStaticEntity';
-export { ItemEntity } from './Entity/ItemEntity/ItemEntity';
-export { HealingGemItemEntity } from './Entity/ItemEntity/FoodItemEntity/HealingGemItemEntity/HealingGemItemEntity';
 export { DynamicEntity } from './Entity/DynamicEntity/DynamicEntity';
-export { NpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/NpcDynamicEntity';
-export { FriendlyNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/FriendlyNpcDynamicEntity/FriendlyNpcDynamicEntity';
-export { NeutralNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/NeutralNpcDynamicEntity/NeutralNpcDynamicEntity';
-export { HostileNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/HostileNpcDynamicEntity/HostileNpcDynamicEntity';
-export { WhitePixelEntity } from './Entity/DynamicEntity/NpcDynamicEntity/HostileNpcDynamicEntity/WhitePixelEntity/WhitePixelEntity';
-export { PlayerDynamicEntity } from './Entity/DynamicEntity/PlayerDynamicEntity/PlayerDynamicEntity';
-export { GrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/GrenadeDynamicEntity';
-export { SmokeGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/SmokeGrenadeDynamicEntity/SmokeGrenadeDynamicEntity';
-export { FragGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/FragGrenadeDynamicEntity/FragGrenadeDynamicEntity';
-export { StunGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/StunGrenadeDynamicEntity/StunGrenadeDynamicEntity';
+// BulletDynamicEntity and its subclasses
 export { BulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/BulletDynamicEntity';
 export { BuckshotBulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/BuckshotBulletDynamicEntity/BuckshotBulletDynamicEntity';
-export { OrdinaryBulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/OrdinaryBulletDynamicEntity/OrdinaryBulletDynamicEntity';
 export { LaserBulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/LaserBulletDynamicEntity/LaserBulletDynamicEntity';
+export { OrdinaryBulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/OrdinaryBulletDynamicEntity/OrdinaryBulletDynamicEntity';
 export { SniperBulletDynamicEntity } from './Entity/DynamicEntity/BulletDynamicEntity/SniperBulletDynamicEntity/SniperBulletDynamicEntity';
-
+// GrenadeDynamicEntity and its subclasses
+export { GrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/GrenadeDynamicEntity';
+export { FragGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/FragGrenadeDynamicEntity/FragGrenadeDynamicEntity';
+export { SmokeGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/SmokeGrenadeDynamicEntity/SmokeGrenadeDynamicEntity';
+export { StunGrenadeDynamicEntity } from './Entity/DynamicEntity/GrenadeDynamicEntity/StunGrenadeDynamicEntity/StunGrenadeDynamicEntity';
+// NpcDynamicEntity and its subclasses
+export { NpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/NpcDynamicEntity';
+export { FriendlyNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/FriendlyNpcDynamicEntity/FriendlyNpcDynamicEntity';
+// HostileNpcDynamicEntity and its subclasses
+export { HostileNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/HostileNpcDynamicEntity/HostileNpcDynamicEntity';
+export { WhitePixelEntity } from './Entity/DynamicEntity/NpcDynamicEntity/HostileNpcDynamicEntity/WhitePixelEntity/WhitePixelEntity';
+// NeutralNpcDynamicEntity
+export { NeutralNpcDynamicEntity } from './Entity/DynamicEntity/NpcDynamicEntity/NeutralNpcDynamicEntity/NeutralNpcDynamicEntity';
+// PlayerDynamicEntity
+export { PlayerDynamicEntity } from './Entity/DynamicEntity/PlayerDynamicEntity/PlayerDynamicEntity';
+// EmptyEntity
+export { EmptyEntity } from './Entity/EmptyEntity/EmptyEntity';
+// ItemEntity and its subclasses
+export { ItemEntity } from './Entity/ItemEntity/ItemEntity';
+export { FoodItemEntity } from './Entity/ItemEntity/FoodItemEntity/FoodItemEntity';
+export { HealingGemItemEntity } from './Entity/ItemEntity/FoodItemEntity/HealingGemItemEntity/HealingGemItemEntity';
+// StaticEntity and its subclasses
+export { StaticEntity } from './Entity/StaticEntity/StaticEntity';
+export { BoxStaticEntity } from './Entity/StaticEntity/BoxStaticEntity/BoxStaticEntity';
+export { CurbStaticEntity } from './Entity/StaticEntity/CurbStaticEntity/CurbStaticEntity';
+export { WallStaticEntity } from './Entity/StaticEntity/WallStaticEntity/WallStaticEntity';
 
 export const PrototypeChain = {
   "CursorManager": "CursorManager",
   "EffectManager": "EffectManager",
   "Entity": {
-    "StaticEntity": {
-      "BoxStaticEntity": "BoxStaticEntity",
-      "WallStaticEntity": "WallStaticEntity",
-      "CurbStaticEntity": "CurbStaticEntity"
+    "DynamicEntity": {
+      "BulletDynamicEntity": {
+        "BuckshotBulletDynamicEntity": "BuckshotBulletDynamicEntity",
+        "LaserBulletDynamicEntity": "LaserBulletDynamicEntity",
+        "OrdinaryBulletDynamicEntity": "OrdinaryBulletDynamicEntity",
+        "SniperBulletDynamicEntity": "SniperBulletDynamicEntity"
+      },
+      "GrenadeDynamicEntity": {
+        "FragGrenadeDynamicEntity": "FragGrenadeDynamicEntity",
+        "SmokeGrenadeDynamicEntity": "SmokeGrenadeDynamicEntity",
+        "StunGrenadeDynamicEntity": "StunGrenadeDynamicEntity"
+      },
+      "NpcDynamicEntity": {
+        "FriendlyNpcDynamicEntity": "FriendlyNpcDynamicEntity",
+        "HostileNpcDynamicEntity": {
+          "WhitePixelEntity": "WhitePixelEntity"
+        },
+        "NeutralNpcDynamicEntity": "NeutralNpcDynamicEntity"
+      },
+      "PlayerDynamicEntity": "PlayerDynamicEntity"
     },
+    "EmptyEntity": "EmptyEntity",
     "ItemEntity": {
       "FoodItemEntity": {
         "HealingGemItemEntity": "HealingGemItemEntity"
       }
     },
-    "DynamicEntity": {
-      "NpcDynamicEntity": {
-        "FriendlyNpcDynamicEntity": "FriendlyNpcDynamicEntity",
-        "NeutralNpcDynamicEntity": "NeutralNpcDynamicEntity",
-        "HostileNpcDynamicEntity": {
-          "WhitePixelEntity": "WhitePixelEntity"
-        }
-      },
-      "PlayerDynamicEntity": "PlayerDynamicEntity",
-      "GrenadeDynamicEntity": {
-        "SmokeGrenadeDynamicEntity": "SmokeGrenadeDynamicEntity",
-        "FragGrenadeDynamicEntity": "FragGrenadeDynamicEntity",
-        "StunGrenadeDynamicEntity": "StunGrenadeDynamicEntity"
-      },
-      "BulletDynamicEntity": {
-        "BuckshotBulletDynamicEntity": "BuckshotBulletDynamicEntity",
-        "OrdinaryBulletDynamicEntity": "OrdinaryBulletDynamicEntity",
-        "LaserBulletDynamicEntity": "LaserBulletDynamicEntity",
-        "SniperBulletDynamicEntity": "SniperBulletDynamicEntity"
-      }
-    },
-    "EmptyEntity": "EmptyEntity"
+    "StaticEntity": {
+      "BoxStaticEntity": "BoxStaticEntity",
+      "CurbStaticEntity": "CurbStaticEntity",
+      "WallStaticEntity": "WallStaticEntity"
+    }
   }
 };
