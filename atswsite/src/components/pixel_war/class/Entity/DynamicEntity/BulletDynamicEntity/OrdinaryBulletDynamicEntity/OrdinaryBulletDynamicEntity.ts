a705f42275909762499abef597d505717dd79de2
emilia-t <sophietwilight@qq.com>
2026-06-21 22:35:07 +0800

    Added OrdinaryBullet trail effect

--- atswsite/src/components/pixel_war/class/Entity/DynamicEntity/BulletDynamicEntity/OrdinaryBulletDynamicEntity/OrdinaryBulletDynamicEntity.ts
+++ atswsite/src/components/pixel_war/class/Entity/DynamicEntity/BulletDynamicEntity/OrdinaryBulletDynamicEntity/OrdinaryBulletDynamicEntity.ts
@@ -22,8 +22,29 @@ class OrdinaryBulletDynamicEntity extends BulletDynamicEntity {
     ctx.translate(screenPos.x, screenPos.y);
     const angle = this.getScreenRotationAngle();
     ctx.rotate(angle);
+
+    const bodyWidth = BulletDynamicEntity.WIDTH;
+    const bodyHeight = BulletDynamicEntity.HEIGHT / 2;
+    const trailLength = bodyWidth * 3.5;
+    const trailWidth = BulletDynamicEntity.HEIGHT * 1.4;
+    const trailGradient = ctx.createLinearGradient(-trailLength, 0, bodyWidth / 2, 0);
+    trailGradient.addColorStop(0, 'rgba(255, 255, 255, 0)');
+    trailGradient.addColorStop(0.55, 'rgba(255, 255, 255, 0.16)');
+    trailGradient.addColorStop(1, this.bulletColor);
+
+    ctx.fillStyle = trailGradient;
+    ctx.beginPath();
+    ctx.moveTo(-trailLength, 0);
+    ctx.quadraticCurveTo(-bodyWidth, -trailWidth / 2, bodyWidth / 2, -bodyHeight / 2);
+    ctx.lineTo(bodyWidth / 2, bodyHeight / 2);
+    ctx.quadraticCurveTo(-bodyWidth, trailWidth / 2, -trailLength, 0);
+    ctx.closePath();
+    ctx.fill();
+
+    ctx.shadowBlur = 8;
+    ctx.shadowColor = this.bulletColor;
     ctx.fillStyle = this.bulletColor;
-    ctx.fillRect(-BulletDynamicEntity.WIDTH / 2, -BulletDynamicEntity.HEIGHT / 4, BulletDynamicEntity.WIDTH, BulletDynamicEntity.HEIGHT / 2);
+    ctx.fillRect(-bodyWidth / 2, -bodyHeight / 2, bodyWidth, bodyHeight);
     ctx.restore();
   }
 }
